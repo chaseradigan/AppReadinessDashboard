739c001747c4ebfae67583e96de24684ed744d0d
Sushma Knavoor Madathil <sushma.knavo@hcl.com>
2020-01-21 13:13:57 -0800

    Semantic Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+
+import { List } from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
+    <div>
+    <List>
+    <List.Item>
+      <List.Icon name='users' />
+      <List.Content>Semantic UI</List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='marker' />
+      <List.Content>New York, NY</List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='mail' />
+      <List.Content>
+        <a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='linkify' />
+      <List.Content>
+        <a href='http://www.semantic-ui.com'>semantic-ui.com</a>
+      </List.Content>
+    </List.Item>
+  </List>
     </div>
   );
 }
